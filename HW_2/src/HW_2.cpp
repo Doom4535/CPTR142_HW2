9a549df85b26c24c02e480b9fe24b5ac86ce7777
Doom4535 <aarcov@gmail.com>
2017-01-17 12:29:09 -0800

    Adding in problem 6.7

--- HW_2/src/HW_2.cpp
+++ HW_2/src/HW_2.cpp
@@ -8,12 +8,15 @@
 
 #include <iostream>
 #include "odometer.h"
+#include "pizza.h"
 
-int prob_1(void);
+int prob_1(void); // #6
+int prob_2(void); // #7
 
 int main() {
 	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
 	prob_1();
+	prob_2();
 	return 0;
 }
 
@@ -46,5 +49,13 @@ int prob_1(void){
 	car1.addOdometer(2200);
 	std::cout << "Trip done, total fuel used: " << car1.getFuel_consumed() << " gallons" << std::endl;
 
+	return(0);
+}
+
+int prob_2(void){
+	std::cout << "Starting Problem 2 (Ch. 6, #7)" << std::endl;
+	Pizza p1; // Making a premade pizza for quick order
+	std::cout << "Price for quick order pizza, type: " << p1.getType() << ", size: " << p1.getSize() << ", number of Toppings: " << p1.get_num_Toppings() << std::endl;
+	std::cout << "Price: " << p1.getPrice();
 	return(0);
 }
