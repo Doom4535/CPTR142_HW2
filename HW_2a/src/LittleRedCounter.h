5d636a5b47bb8603a453171fb268711094eb1060
Doom4535 <aarcov@gmail.com>
2017-01-23 11:25:42 -0800

    working on adding UML diagram for 2a

--- HW_2a/src/LittleRedCounter.h
+++ HW_2a/src/LittleRedCounter.h
@@ -8,6 +8,23 @@
 #ifndef LITTLEREDCOUNTER_H_
 #define LITTLEREDCOUNTER_H_
 
+/*
+ * UML Diagram:
+ *  Counter class:
+ *  	Private:
+ *  		count (total value counted)
+ *  		max (the value that the device will overflow at
+ *  		overflow (did the device overflow?, yes/no)
+ *  	Public:
+ *  		create a counter (constructor, default to a value of 0)
+ *  		reset/clear the counter (clear count and overflow)
+ *  		increment by 1 (also check if the addition will cause an overflow)
+ *  		increment by 10 (also check if the addition will cause an overflow)
+ *  		increment by 100 (also check if the addition will cause an overflow)
+ *  		increment by 1000 (also check if the addition will cause an overflow)
+ *  		check if overflow set
+ */
+
 class Counter{
 	private:
 		int count;
